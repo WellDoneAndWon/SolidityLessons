cd7b27f9fab8e2f8d8b614354901fc5dd5437d16
WellDoneAndWon <ildanaxy@gmail.com>
2023-01-29 01:38:07 +0500

    WellDone

--- lesson_4_enum_array_byte_struct/Demo.sol
+++ lesson_4_enum_array_byte_struct/Demo.sol
@@ -58,4 +58,50 @@ contract Demo {
         tempArray[2] = 1;
         return tempArray;
     }
+
+    //byte
+    bytes32 public myVar = "test here";
+    // 1 --> 32
+    // 32 * 8 = 256
+    // uint256
+    bytes public myDynVar = "test here";
+    bytes public myDynKirilVar = unicode"привет мир!";
+
+    function lengthBytesArray() public view returns(uint) {
+        return myDynVar.length; //работает хорошо для букв латинского алфавита
+    }
+
+    //struct
+    struct Payment {
+        uint amount;
+        uint timestamp;
+        address from;
+        string message;
+    }
+
+    struct Balance {
+        uint totalPayments;
+        mapping (uint => Payment) payments;
+    }
+
+    mapping (address => Balance) public balances;
+
+    function getPayment(address _addr, uint _index) public view returns(Payment memory) {
+        return balances[_addr].payments[_index];
+    }
+
+    function pay(string memory message) public payable {
+        uint paymentNum = balances[msg.sender].totalPayments;
+        
+        balances[msg.sender].totalPayments++;
+
+        Payment memory newPayment = Payment(
+            msg.value,
+            block.timestamp,
+            msg.sender,
+            message
+        );
+    
+        balances[msg.sender].payments[paymentNum] = newPayment;
+    }
 }
