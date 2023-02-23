dceb272e86808eb38fb03b6a74a346e58c19df9a
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-24 01:48:00 +0500

    added fn buy&withdrow and modifiers

--- lesson_10_auctionEngine/contracts/AucEngine.sol
+++ lesson_10_auctionEngine/contracts/AucEngine.sol
@@ -19,9 +19,20 @@ contract AucEngine {
     Auction[] public auctions;
     
     event AuctionCreated(uint index, string itemName, uint startingPrice, uint duration);
+    event AuctionEnded(address seller, address winner, uint price)
 
     constructor() {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not admin!");
+        _;
+    }
+
+    modifier notStopped() {
+        require(!currentAuction.stopped, "Auction was stopped!");
+        _;
     }
 
     function createAuction(uint _startingPrice, uint _discountRate, string calldata _item, uint _duration) external {
@@ -45,12 +56,29 @@ contract AucEngine {
         emit AuctionCreated(auctions.length - 1, _item, _startingPrice, duration);
     }
 
-    function getPriceFor(uint index) public view returns(uint) {
+    function getPriceFor(uint index) public view returns(uint) notStopped() {
         Auction memory currentAuction = auctions[index];
         uint elapsed = block.timestamp - currentAuction.startsAt;
         uint discount = elapsed * currentAuction.discountRate;
         return currentAuction.startingPrice - discount;
     }
 
-    
+    function buy(uint index) external payable notStopped() {
+        Auction memory currentAuction = auctions[index];
+        require(block.timestamp <= currentAuction.endsAt, "Auction was closed!");
+        int currentPrice = getPriceFor(index);
+        require(msg.value >= currentPrice,"You are trying transfer not enougth funds!");
+        currentAuction.finalPrice = currentPrice;
+        currentAuction.stopped = true;
+        uint refund = msg.value - currentPrice;
+        if (refund > 0) {
+            (payable)msg.sender.transfer(refund);
+        }
+        currentAuction.seller.transfer(currentPrice - (currentPrice * FEE)/100);
+        emit AuctionEnded(currentAuction.seller, msg.sender, currentPrice)
+    }
+
+    function withdrowAllFees() external onlyOwner() {
+        (payable)msg.sender.transfer(this.balance);
+    }
 }
